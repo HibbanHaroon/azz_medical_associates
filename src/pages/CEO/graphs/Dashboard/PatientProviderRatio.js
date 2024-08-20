dcf2110340f21af7435e99eea8d84cfc16362cd8
HibbanHaroon <hibbanharoon@gmail.com>
2024-08-20 15:33:58 +0500

    Add forwardRef to PatientProviderRatio, PatientsPerDay components

--- src/pages/CEO/graphs/Dashboard/PatientProviderRatio.js
+++ src/pages/CEO/graphs/Dashboard/PatientProviderRatio.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, forwardRef } from "react";
 import { Box, Grid, Typography } from "@mui/material";
 import { Bar } from "react-chartjs-2";
 import "chart.js/auto";
@@ -8,154 +8,150 @@ import { Chart } from "chart.js";
 
 Chart.register(ChartDataLabels);
 
-function PatientProviderRatio({
-  clinics,
-  arrivals,
-  doctors,
-  patientProviderRatioRef,
-  onDataProcessed,
-}) {
-  const [chartData, setChartData] = useState({ datasets: [] });
-  const theme = useTheme();
+const PatientProviderRatio = forwardRef(
+  ({ clinics, arrivals, doctors, onDataProcessed }, ref) => {
+    const [chartData, setChartData] = useState({ datasets: [] });
+    const theme = useTheme();
 
-  const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    scales: {
-      x: {
-        display: false,
-        grid: {
+    const options = {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        x: {
+          display: false,
+          grid: {
+            display: false,
+          },
+        },
+        y: {
+          display: true,
+          beginAtZero: true,
+          ticks: {
+            stepSize: 1,
+            callback: function (value) {
+              if (Number.isInteger(value)) {
+                return value;
+              }
+            },
+          },
+        },
+      },
+      plugins: {
+        legend: {
           display: false,
         },
-      },
-      y: {
-        display: true,
-        beginAtZero: true,
-        ticks: {
-          stepSize: 1,
-          callback: function (value) {
-            if (Number.isInteger(value)) {
-              return value;
-            }
+        tooltip: {
+          callbacks: {
+            label: function (context) {
+              let label = context.dataset.label || "";
+              if (label) {
+                label += ": ";
+              }
+              if (context.parsed.y !== null) {
+                label += context.parsed.y;
+              }
+              return label;
+            },
           },
         },
-      },
-    },
-    plugins: {
-      legend: {
-        display: false,
-      },
-      tooltip: {
-        callbacks: {
-          label: function (context) {
-            let label = context.dataset.label || "";
-            if (label) {
-              label += ": ";
+        datalabels: {
+          anchor: "end",
+          align: "start",
+          formatter: (value, context) => {
+            if (value === 0) {
+              return "";
             }
-            if (context.parsed.y !== null) {
-              label += context.parsed.y;
-            }
-            return label;
+            return context.chart.data.labels[context.dataIndex];
           },
+          font: {
+            size: 10,
+          },
+          color: theme.palette.text.primary,
+          rotation: 0,
+          offset: -15,
         },
       },
-      datalabels: {
-        anchor: "end",
-        align: "start",
-        formatter: (value, context) => {
-          if (value === 0) {
-            return "";
+      elements: {
+        point: {
+          radius: 5,
+        },
+      },
+    };
+
+    useEffect(() => {
+      const getClinicRatios = () => {
+        const names = [];
+        const ratios = [];
+
+        for (const clinic of clinics) {
+          const clinicArrivals = arrivals.filter(
+            (arrival) => arrival.clinicId === clinic.id
+          );
+          const clinicDoctors = doctors.filter(
+            (doctor) => doctor.clinicId === clinic.id
+          );
+
+          let ratio = 0;
+          if (clinicDoctors.length > 0) {
+            ratio = clinicArrivals.length / clinicDoctors.length;
           }
-          return context.chart.data.labels[context.dataIndex];
-        },
-        font: {
-          size: 10,
-        },
-        color: theme.palette.text.primary,
-        rotation: 0,
-        offset: -15,
-      },
-    },
-    elements: {
-      point: {
-        radius: 5,
-      },
-    },
-  };
-
-  useEffect(() => {
-    const getClinicRatios = () => {
-      const names = [];
-      const ratios = [];
-
-      for (const clinic of clinics) {
-        const clinicArrivals = arrivals.filter(
-          (arrival) => arrival.clinicId === clinic.id
-        );
-        const clinicDoctors = doctors.filter(
-          (doctor) => doctor.clinicId === clinic.id
-        );
-
-        let ratio = 0;
-        if (clinicDoctors.length > 0) {
-          ratio = clinicArrivals.length / clinicDoctors.length;
+          names.push(clinic.name);
+          ratios.push(parseInt(ratio, 10));
         }
-        names.push(clinic.name);
-        ratios.push(parseInt(ratio, 10));
-      }
 
-      return { names, ratios };
-    };
+        return { names, ratios };
+      };
 
-    const { names, ratios } = getClinicRatios();
+      const { names, ratios } = getClinicRatios();
 
-    const data = {
-      labels: names,
-      datasets: [
-        {
-          label: "Arrivals to Providers Ratio",
-          data: ratios,
-          backgroundColor: theme.palette.primary.main,
-          borderColor: theme.palette.primary.dark,
-          borderWidth: 0,
-          barThickness: 20,
-          categoryPercentage: 0.8,
-          barPercentage: 0.9,
-          borderRadius: 40,
-        },
-      ],
-    };
+      const data = {
+        labels: names,
+        datasets: [
+          {
+            label: "Arrivals to Providers Ratio",
+            data: ratios,
+            backgroundColor: theme.palette.primary.main,
+            borderColor: theme.palette.primary.dark,
+            borderWidth: 0,
+            barThickness: 20,
+            categoryPercentage: 0.8,
+            barPercentage: 0.9,
+            borderRadius: 40,
+          },
+        ],
+      };
 
-    setChartData(data);
-    onDataProcessed();
-  }, [
-    clinics,
-    arrivals,
-    doctors,
-    theme.palette.primary.main,
-    theme.palette.primary.dark,
-    onDataProcessed,
-  ]);
+      setChartData(data);
+      onDataProcessed();
+    }, [
+      clinics,
+      arrivals,
+      doctors,
+      theme.palette.primary.main,
+      theme.palette.primary.dark,
+      onDataProcessed,
+    ]);
 
-  return (
-    <Grid item xs={12} md={6}>
-      <Box
-        sx={{ p: 3, m: 1, borderRadius: 3, boxShadow: 2, height: 300 }}
-        ref={patientProviderRatioRef}
-      >
-        <Typography
-          variant="h6"
-          fontWeight="bold"
-          sx={{ marginBottom: 0, marginTop: 0 }}
+    return (
+      <Grid item xs={12} md={6}>
+        <Box
+          sx={{ p: 3, m: 1, borderRadius: 3, boxShadow: 2, height: 300 }}
+          ref={ref}
         >
-          Patient Provider Ratio
-        </Typography>
-        <div style={{ width: "100%", height: "95%" }}>
-          <Bar data={chartData} options={options} />
-        </div>
-      </Box>
-    </Grid>
-  );
-}
+          <Typography
+            variant="h6"
+            fontWeight="bold"
+            sx={{ marginBottom: 0, marginTop: 0 }}
+          >
+            Patient Provider Ratio
+          </Typography>
+          <div style={{ width: "100%", height: "95%" }}>
+            <Bar data={chartData} options={options} />
+          </div>
+        </Box>
+      </Grid>
+    );
+  }
+);
 
 export default PatientProviderRatio;
--- src/pages/CEO/graphs/Dashboard/PatientsPerDay.js
+++ src/pages/CEO/graphs/Dashboard/PatientsPerDay.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, forwardRef } from "react";
 import { Box, Grid, Typography } from "@mui/material";
 import { Bar } from "react-chartjs-2";
 import {
@@ -21,153 +21,149 @@ ChartJS.register(
   Legend
 );
 
-const PatientsPerDay = ({
-  clinics,
-  doctors,
-  arrivals,
-  patientsPerDayRef,
-  onDataProcessed,
-}) => {
-  const theme = useTheme();
-  const [dataForOneMonthArrivals, setDataForOneMonthArrivals] = useState([]);
+const PatientsPerDay = forwardRef(
+  ({ clinics, doctors, arrivals, onDataProcessed }, ref) => {
+    const theme = useTheme();
+    const [dataForOneMonthArrivals, setDataForOneMonthArrivals] = useState([]);
 
-  useEffect(() => {
-    const processData = () => {
-      // Process and calculate today's arrivals per clinic
-      const todayArrivalsPerClinic = clinics.map((clinic) => {
-        // Filter doctors specific to the clinic
-        const clinicDoctors = doctors.filter(
-          (doc) => doc.clinicId === clinic.id
-        );
-
-        // Count today's arrivals for each doctor in the clinic
-        const todayArrivalsCount = clinicDoctors.reduce((acc, doctor) => {
-          const doctorArrivals = arrivals.filter(
-            (arrival) =>
-              arrival.clinicId === clinic.id &&
-              arrival.doctorID === doctor.id &&
-              isToday(arrival.arrivalTime)
+    useEffect(() => {
+      const processData = () => {
+        // Process and calculate today's arrivals per clinic
+        const todayArrivalsPerClinic = clinics.map((clinic) => {
+          // Filter doctors specific to the clinic
+          const clinicDoctors = doctors.filter(
+            (doc) => doc.clinicId === clinic.id
           );
-          return acc + doctorArrivals.length;
-        }, 0);
 
-        return {
-          clinicName: clinic.name,
-          todayArrivalsCount,
-        };
-      });
+          // Count today's arrivals for each doctor in the clinic
+          const todayArrivalsCount = clinicDoctors.reduce((acc, doctor) => {
+            const doctorArrivals = arrivals.filter(
+              (arrival) =>
+                arrival.clinicId === clinic.id &&
+                arrival.doctorID === doctor.id &&
+                isToday(arrival.arrivalTime)
+            );
+            return acc + doctorArrivals.length;
+          }, 0);
 
-      // Sort by today's arrival count in descending order and select the top 6
-      const sortedData = todayArrivalsPerClinic
-        .sort((a, b) => b.todayArrivalsCount - a.todayArrivalsCount)
-        .slice(0, 6);
+          return {
+            clinicName: clinic.name,
+            todayArrivalsCount,
+          };
+        });
 
-      setDataForOneMonthArrivals(sortedData);
-      onDataProcessed();
+        // Sort by today's arrival count in descending order and select the top 6
+        const sortedData = todayArrivalsPerClinic
+          .sort((a, b) => b.todayArrivalsCount - a.todayArrivalsCount)
+          .slice(0, 6);
+
+        setDataForOneMonthArrivals(sortedData);
+        onDataProcessed();
+      };
+
+      // Helper function to check if a date is today
+      const isToday = (dateString) => {
+        const date = new Date(dateString);
+        const today = new Date();
+        return (
+          date.getDate() === today.getDate() &&
+          date.getMonth() === today.getMonth() &&
+          date.getFullYear() === today.getFullYear()
+        );
+      };
+
+      processData();
+    }, [clinics, doctors, arrivals, onDataProcessed]);
+
+    const highestValue = dataForOneMonthArrivals[0]?.todayArrivalsCount;
+
+    const chartData = {
+      labels: dataForOneMonthArrivals.map((item) => item.clinicName),
+      datasets: [
+        {
+          data: dataForOneMonthArrivals.map((item) => item.todayArrivalsCount),
+          backgroundColor: dataForOneMonthArrivals.map((item) =>
+            item.todayArrivalsCount === highestValue
+              ? "#36A2EB"
+              : theme.palette.primary.main
+          ),
+          borderColor: theme.palette.primary.dark,
+          borderWidth: 1,
+          borderRadius: 5,
+          barThickness: 10,
+        },
+      ],
     };
 
-    // Helper function to check if a date is today
-    const isToday = (dateString) => {
-      const date = new Date(dateString);
-      const today = new Date();
-      return (
-        date.getDate() === today.getDate() &&
-        date.getMonth() === today.getMonth() &&
-        date.getFullYear() === today.getFullYear()
-      );
-    };
-
-    processData();
-  }, [clinics, doctors, arrivals, onDataProcessed]);
-
-  const highestValue = dataForOneMonthArrivals[0]?.todayArrivalsCount;
-
-  const chartData = {
-    labels: dataForOneMonthArrivals.map((item) => item.clinicName),
-    datasets: [
-      {
-        data: dataForOneMonthArrivals.map((item) => item.todayArrivalsCount),
-        backgroundColor: dataForOneMonthArrivals.map((item) =>
-          item.todayArrivalsCount === highestValue
-            ? "#36A2EB"
-            : theme.palette.primary.main
-        ),
-        borderColor: theme.palette.primary.dark,
-        borderWidth: 1,
-        borderRadius: 5,
-        barThickness: 10,
-      },
-    ],
-  };
-
-  const options = {
-    indexAxis: "y",
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-      legend: {
-        display: false,
-      },
-      tooltip: {
-        enabled: true,
-        callbacks: {
-          label: function (context) {
-            return `${context.label}: ${context.parsed.x}`;
+    const options = {
+      indexAxis: "y",
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          display: false,
+        },
+        tooltip: {
+          enabled: true,
+          callbacks: {
+            label: function (context) {
+              return `${context.label}: ${context.parsed.x}`;
+            },
           },
         },
       },
-    },
-    scales: {
-      x: {
-        grid: {
-          display: false,
-        },
-        ticks: {
-          display: true,
-        },
-      },
-      y: {
-        grid: {
-          display: false,
-        },
-        ticks: {
-          display: true,
-          mirror: false,
-          color: "#000",
-          font: {
-            size: 14,
+      scales: {
+        x: {
+          grid: {
+            display: false,
           },
-          padding: 10,
+          ticks: {
+            display: true,
+          },
         },
-        barPercentage: 0.8,
-        categoryPercentage: 1.0,
+        y: {
+          grid: {
+            display: false,
+          },
+          ticks: {
+            display: true,
+            mirror: false,
+            color: "#000",
+            font: {
+              size: 14,
+            },
+            padding: 10,
+          },
+          barPercentage: 0.8,
+          categoryPercentage: 1.0,
+        },
       },
-    },
-    animation: {
-      duration: 200,
-      easing: "easeInOutBounce",
-    },
-  };
+      animation: {
+        duration: 200,
+        easing: "easeInOutBounce",
+      },
+    };
 
-  return (
-    <Grid item xs={12} md={6}>
-      <Box
-        sx={{ p: 3, m: 1, borderRadius: 3, boxShadow: 2, height: 300 }}
-        ref={patientsPerDayRef}
-      >
-        <Typography
-          variant="h6"
-          fontWeight="bold"
-          sx={{ marginBottom: 0, marginTop: 0 }}
+    return (
+      <Grid item xs={12} md={6}>
+        <Box
+          sx={{ p: 3, m: 1, borderRadius: 3, boxShadow: 2, height: 300 }}
+          ref={ref}
         >
-          Patients Per Day
-        </Typography>
-        <Box sx={{ width: "100%", height: "240px" }}>
-          <Bar data={chartData} options={options} />
+          <Typography
+            variant="h6"
+            fontWeight="bold"
+            sx={{ marginBottom: 0, marginTop: 0 }}
+          >
+            Patients Per Day
+          </Typography>
+          <Box sx={{ width: "100%", height: "240px" }}>
+            <Bar data={chartData} options={options} />
+          </Box>
         </Box>
-      </Box>
-    </Grid>
-  );
-};
+      </Grid>
+    );
+  }
+);
 
 export default PatientsPerDay;
